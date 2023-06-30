b82c0d50d93c494a1e68343a15aac481f8a2dc92
SanjaySamaa <bistasanjay7@gmail.com>
2023-06-30 13:28:40 +0545

    revising some javascripts

---
+++ basicJavascript/one.js
@@ -0,0 +1,35 @@
+// function bottleToBuy(money){
+//     var price = 50;
+//     var numberOfBottels = Math.floor(money / price);
+//     console.log("you can buy "+numberOfBottels+" bottles of milk.");
+//     return money % price;
+// }
+// console.log("you have change of "+bottleToBuy(205));
+// var weight = prompt("enter the weight is kg");
+// var height = prompt("enter the height in meter");
+// function bmiCalculator(weight,height){
+//   var bmi = Math.round(weight/(Math.pow(height,2)));
+//   return bmi;
+// }
+// alert(bmiCalculator(weight,height));
+
+// function randomNumber(){
+//   var rand = Math.ceil(Math.random()*100);
+//   return rand;
+// }
+
+// var name = prompt("enter your name");
+// var pname=prompt("enter the name of your partner or crush");
+
+// console.log("Love percentage of "+name+" and "+pname+" is "+randomNumber()+" %");
+// function randomNumber(){
+//   var rand = Math.ceil(Math.random()*2);
+//   return rand;
+// }
+// for( i=0;i<=10000;i++){
+//   var ran = randomNumber();
+//   if(ran == 0){
+//     alert("0 came in the code");
+//     break;
+//   }
+// }
