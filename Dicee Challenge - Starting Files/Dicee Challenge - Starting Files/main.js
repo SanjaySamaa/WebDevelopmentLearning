a4f09ce09eaf9befd0264e6e81b6e39bdad24134
SanjaySamaa <bistasanjay7@gmail.com>
2023-07-03 17:44:14 +0545

    starting drum project

--- Dicee Challenge - Starting Files/Dicee Challenge - Starting Files/main.js
+++ Dicee Challenge - Starting Files/Dicee Challenge - Starting Files/main.js
@@ -13,35 +13,37 @@ function play(){
     }else{
         document.querySelector(".container h1").textContent="Player Two Wins";
     }
-    var text;
-    if(playerOneScore === 1 ){
-        text = "dice1"
-    }else if(playerOneScore === 2 ){
-        text = "dice2"
-    }else if(playerOneScore === 3 ){
-        text = "dice3"
-    }else if(playerOneScore === 4 ){
-        text = "dice4"
-    }else if(playerOneScore === 5 ){
-        text = "dice5"
-    }else{
-        text = "dice6"
-    }
-    var text2;
-    if(playerTwoScore === 1 ){
-        text2 = "dice1"
-    }else if(playerTwoScore === 2 ){
-        text2 = "dice2"
-    }else if(playerTwoScore === 3 ){
-        text2 = "dice3"
-    }else if(playerTwoScore === 4 ){
-        text2 = "dice4"
-    }else if(playerTwoScore === 5 ){
-        text2 = "dice5"
-    }else{
-        text2 = "dice6"
-    }
-    document.querySelector(".img1").setAttribute("src","./images/"+text+".png");
-    document.querySelector(".img2").setAttribute("src","./images/"+text2+".png");
+    // var text;
+    // if(playerOneScore === 1 ){
+    //     text = "dice1"
+    // }else if(playerOneScore === 2 ){
+    //     text = "dice2"
+    // }else if(playerOneScore === 3 ){
+    //     text = "dice3"
+    // }else if(playerOneScore === 4 ){
+    //     text = "dice4"
+    // }else if(playerOneScore === 5 ){
+    //     text = "dice5"
+    // }else{
+    //     text = "dice6"
+    // }
+    // var text2;
+    // if(playerTwoScore === 1 ){
+    //     text2 = "dice1"
+    // }else if(playerTwoScore === 2 ){
+    //     text2 = "dice2"
+    // }else if(playerTwoScore === 3 ){
+    //     text2 = "dice3"
+    // }else if(playerTwoScore === 4 ){
+    //     text2 = "dice4"
+    // }else if(playerTwoScore === 5 ){
+    //     text2 = "dice5"
+    // }else{
+    //     text2 = "dice6"
+    // }
+    document.querySelector(".img1").setAttribute("src","./images/dice"+playerOneScore+".png");
+    document.querySelector(".img2").setAttribute("src","./images/dice"+playerTwoScore+".png");
+    //aslo can be don using querySelectorAll []
+
 } 
 document.querySelector("body").onload = play();
