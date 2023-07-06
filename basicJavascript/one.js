66373ed99923ded1ce82c4aafa9accf441a98551
SanjaySamaa <bistasanjay7@gmail.com>
2023-07-06 07:03:34 +0545

    finished jQuery

--- basicJavascript/one.js
+++ basicJavascript/one.js
@@ -132,3 +132,117 @@
     //     return operator(num1,num2);
     // }
     // calculator(5,5,add);
+    // function anotherAddEventListener(typeOfEvent,callBack){
+    //     var obj = {
+    //         eventType : "click",
+    //         duration : 2,
+    //         key : "a"
+    //     }
+    //     if(obj.eventType === typeOfEvent){
+    //         callBack(obj.eventType);
+    //     }
+    // }
+    // anotherAddEventListener("click",function(e){
+    //     console.log(e);
+    // })
+    // var number = [];
+
+// var count = 1;
+
+// function numPush(){
+//  while(count <= 100){   
+// if(count%3===0 && count%5 ===0){
+//     number.push("fizzbuzz");
+//     count++;
+// }else if(count%3===0){
+//     number.push("fizz");
+//     count++;
+// }else if(count % 5 === 0 ){
+//     number.push("buzz");
+//     count++;
+// }else{ 
+//     number.push(count);
+//     count++;
+//     }
+    
+// }}
+// numPush();
+// console.log(number)
+// var number = [1,2,3,4,5];
+// var rand = Math.floor(Math.random()*number.length);
+// console.log(rand);
+// var count = 99;
+// function song(count){
+// while(count >0){
+    
+//     if(count === 1){
+//          console.log(count+" bottles of beer on the wall, "+count+" bottles of beer. \nTake one down and pass it around, no bottles of beer on the wall.");       
+//     }else{
+//         console.log(count+" bottles of beer on the wall, "+count+" bottles of beer. \nTake one down and pass it around, "+(count-1)+" bottles of beer on the wall.");
+//         }
+//     count--;
+// }}
+// song(count);
+// function divide(num1,num2){
+//     return num1/num2;
+// }
+// function multiply(num1,num2){
+//     return num1*num2;
+// }
+// function add(num1,num2){
+//     return num1+num2;
+// }
+// function subtract(num1,num2){
+//     return num1-num2;
+// }
+// function calculator (num1,num2,operator){
+//     return operator(num1,num2);
+// }
+// calculator(5,5,add);
+// var engineer = {
+//     name:"ram",
+//     age:"40",
+//     number:"9828192819",
+//     languager:["english","nepali"],
+//     experienceYear:12
+    
+// }
+// console.log(engineer.experience);
+// function Engineer (name,age,yearsOfExperience){
+//     this.name = name;
+//     this.age = age;
+//     this.yearsOfExperience = yearsOfExperience;
+//     this.buildBridge = function () {
+//         console.log("builded");
+//     }
+// }
+// var e1 = new Engineer("ram",20,12);
+// var e2 = new Engineer("shyam",15,2);
+
+// function cB(typeOfEvent,cb){
+
+//     var obj = {
+//         eventType : "click",
+//         duration : 2
+//     };
+//         if(typeOfEvent === obj.eventType){
+//             cb(obj);
+//         }
+
+// }
+//     (cB("click",function(e){
+//     console.log(e);
+// }));
+
+
+// function Engineer(name,age,yearsOfExperience){
+//     this.name = name;
+//     this.age = age;
+//     this.yearsOfExperience = yearsOfExperience;
+//     this.job = function (){
+//         console.log("buildied")
+//     }
+// }
+
+// var e1 = new Engineer("ram",20,12);
+// console.log(e1.job);
--- jQuery/main.js
+++ jQuery/main.js
@@ -1,1 +1,40 @@
-alert("rato hunxa aba");
+//using call for using jquery or javascript when document is loaded
+// $("document").ready(function(){
+    //     $("h1").css("color","blue")
+    // });
+
+    $("button").css("color","red");
+    console.log($("a").attr("href"));//to get the value
+    //always remember one parameter is for getting the value and double parameter is to set the value to given first parameter
+    // $("a").attr("href","https://www.facebook.com");
+    // $("document").ready(function(){$("a").toggleClass("huge");});
+
+    // $("button").click(function (){
+    //     $("button").addClass("huge");
+    //     setTimeout(() => {
+    //         $("button").removeClass("huge");
+    //     }, 100);
+    // })
+
+        //to add event listner to keyboard with jQuery just replace .click method with .keypress method
+        $("input").keypress(function(e){
+            console.log(e.key);
+            $("h1").text("you have entered "+e.key+" in your keyboard");
+        });
+    $("h1").on("mouseover",function(){
+        $("h1").css("color","blue");
+        setTimeout(() => {
+            $("h1").css("color","black");
+        }, 1000);
+    })
+    //adding button before h1
+    $("h1").before("<button> click </button>")
+    //you can also use .after method to add element after h1
+    $("h1").after("<button> click </button>")
+    //likewise you can also use prepend and append to insert element left of h1 or right of h1 respectively
+    $("h1").prepend("<button> click </button>")//before h1 content inside h1 tag
+    $("h1").append("<button> click </button>")//after h1 content inside h1 tag
+    // $("h1").remove();//to remove all the button
+    $("button").on("click",function(){
+        $("h1").slideUp().slideDown().fadeOut().fadeIn().animate({opacity:0.5});//all can be done sepeately and also have toggle option like slideToggle(); and in animate we can only use properties with number value
+    });
