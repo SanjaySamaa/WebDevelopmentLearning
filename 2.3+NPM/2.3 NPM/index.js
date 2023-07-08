420c44d9c2f86436ab48106fa6686e81539e446f
SanjaySamaa <bistasanjay7@gmail.com>
2023-07-08 13:06:16 +0545

    npm package manager learning

---
+++ 2.3+NPM/2.3 NPM/index.js
@@ -0,0 +1,7 @@
+// const generateName = require("sillyname");
+// import generateName from "sillyname";
+// var sillyName = generateName();
+// console.log(`my name is ${sillyName}`);
+import superheroes from "superheroes";
+var randomSuperHeroes = superheroes.random();
+console.log(randomSuperHeroes);
