b95ce0a5b074b595dc5d21ac7055db8d39d2ec53
SanjaySamaa <bistasanjay7@gmail.com>
2023-07-10 13:49:13 +0545

    learning server side

---
+++ bmi-backend/bmicalculator.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname+"/bmi.html");
+});
+app.post("/",function(req,res){
+    // console.log(req.body);
+        var weight = Number(req.body.weight);
+        var height = Number(req.body.height);
+        var result = Math.round(weight/Math.pow(height,2));
+        res.send(`<h1 style="text-align:center ; color:red;">Your BMI is ${result}</h1>`);
+   
+});
+app.listen(3000,function(){
+    console.log("server started at port 3000");
+});
