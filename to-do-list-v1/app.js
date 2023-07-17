0bf7a76dbd69d1c5a0c055f99423ec8369094597
SanjaySamaa <bistasanjay7@gmail.com>
2023-07-17 08:10:22 +0545

    Start To Do List v1

--- to-do-list-v1/app.js
+++ to-do-list-v1/app.js
@@ -46,37 +46,57 @@ app.use(express.static("public"));
 
 const date = new Date();
 var day = date.getDay();
+var options = {
+        weekday:'long',
+        year:'numeric',
+        month:'long',
+        day:'numeric'
+};
+var item = [];
 
 app.get("/",(req,res)=>{
-    switch(day){
-        case 0: day ="sunday";
-                break;
-        case 1: day ="monday";
-                break;
-        case 2: day ="tuesday";
-                break;
-        case 3: day ="wednesday";
-                break;         
-        case 4: day ="thursday";
-                break;
-        case 5: day ="friday";
-                break;
-        case 6: day ="saturday";
-                break;
-        default:
-                day = "some thing wrong with the code";  
-    }
+        
+//     switch(day){
+//         case 0: day ="sunday";
+//                 break;
+//         case 1: day ="monday";
+//                 break;
+//         case 2: day ="tuesday";
+//                 break;
+//         case 3: day ="wednesday";
+//                 break;         
+//         case 4: day ="thursday";
+//                 break;
+//         case 5: day ="friday";
+//                 break;
+//         case 6: day ="saturday";
+//                 break;
+//         default:
+//                 day = "some thing wrong with the code";  
+//     }
     // sendFile(__dirname+"/index.html");
-    if(day === 'sunday' || day === 'saturday'){
-        // res.write("today is weekend day \n");
-        res.render("lists",{dayOfWeek:day+" and it's a weekend day you can rest for while"});
-        // res.send();
-    }else{
-        // res.write("today is not a weekend day \n");
-        res.render("lists",{dayOfWeek:day});
-        // res.send();
-    }
-  
+//     if(day === 'sunday' || day === 'saturday'){
+//         // res.write("today is weekend day \n");
+//         res.render("lists",{dayOfWeek:day+" and it's a weekend day you can rest for while"});
+//         // res.send();
+//     }else{
+//         // res.write("today is not a weekend day \n");
+//         res.render("lists",{dayOfWeek:day});
+//         // res.send();
+//     }
+        // res.sendFile(__dirname+"/index.html")
+        // console.log(day);
+        
+        day = date.toLocaleDateString("en-US",options);
+        res.render("lists",{dayOfWeek:day,items:item});
 });
-
-app.listen(process.env.PORT,3000,()=>console.log("server started on port 3000"));
+app.post("/",(req,res)=>{
+        var i = req.body.item
+        item.push(i);
+        // res.render("lists",{items:item});
+        // res.send();
+        // console.log(item);
+        res.redirect("/");
+       
+});
+app.listen(process.env.PORT||3000,()=>console.log("server started on port 3000"));
